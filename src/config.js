a4411f9dc2602bf928e9d169dc8387c0b7642cd6
HarshKunte <harshkunte99@gmail.com>
2021-04-20 21:25:22 +0530

    init

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ import 'firebase/storage';
 import 'firebase/auth'
 // import 'firebase/firestore';
 var firebaseConfig = {
-    apiKey: "AIzaSyDLZNGpY7D4dHgmgjB9WuFJExTTAoDkESs",
-    authDomain: "pictitious-751c6.firebaseapp.com",
-    projectId: "pictitious-751c6",
-    storageBucket: "pictitious-751c6.appspot.com",
-    messagingSenderId: "1094919959542",
-    appId: "1:1094919959542:web:ff9650c062694ab0f3cdd7",
-    measurementId: "G-J0G3KJWMBF"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId:  process.env.REACT_APP_MEASUREMENT_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
